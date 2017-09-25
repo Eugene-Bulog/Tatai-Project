b7715a90ac446554efba2c9ec6eec4121422c812
Eugene-Bulog <eugene.bulog@gmail.com>
2017-09-25 16:22:37 +1300

    Changed summary screen from listview to embedded VBox, with color

--- src/gui/Summary.java
+++ src/gui/Summary.java
@@ -1,25 +1,21 @@
 package gui;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import utility.NumberList;
 
 public class Summary extends VBox{
 
-	// The list which will summarize all the questions and answers this session
-	private ListView<String> _answerList = new ListView<String>();
-	private ObservableList<String> _questions = FXCollections.observableArrayList();
 	
 	// The label for score
 	private Label _score;
+	private SummaryBox _summaryBox;
 	
 	// Buttons
 	private Button _mainMenu;
@@ -33,15 +29,7 @@ public class Summary extends VBox{
 		
 		setBackground(App.getPatternBackground());
 		
-		// Adds each question and answer to the list
-		for (int i = 0; i < 10; i++) {
-			_questions.add(NumberList.getNumberAt(i) + ": " + 
-					NumberList.getAnswerAt(i));
-		}
-		
-		// Adds the ObservableList to the ListView
-		_answerList.setItems(_questions);
-		_answerList.setPrefHeight(232);
+		_summaryBox = new SummaryBox();
 		
 		// Set up score label
 		_score = new Label("Score: " + NumberList.getSessionScore() + "/10");
@@ -65,12 +53,12 @@ public class Summary extends VBox{
 		setUpActions();
 		
 		setSpacing(40);
-		this.setAlignment(Pos.CENTER);
+		setAlignment(Pos.CENTER);
 		
 		
 		// Adds the components to the VBox
 		getChildren().add(_score);
-		getChildren().add(_answerList);
+		getChildren().add(_summaryBox);
 		getChildren().add(_playAgain);
 		getChildren().add(_mainMenu);
 		
@@ -95,7 +83,8 @@ public class Summary extends VBox{
 				}
 
 				// Move to the question scene
-				App.getMainStage().setScene(new Scene(new QuestionAsk(),App.APP_WIDTH,App.APP_HEIGHT));
+				App.getMainStage().setScene(new Scene(new QuestionAsk(),
+						App.APP_WIDTH,App.APP_HEIGHT));
 			}
 		});
 		
@@ -103,7 +92,8 @@ public class Summary extends VBox{
 		_mainMenu.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				App.getMainStage().setScene(new Scene(new MainMenu(),App.APP_WIDTH,App.APP_HEIGHT));
+				App.getMainStage().setScene(new Scene(new MainMenu(),
+						App.APP_WIDTH,App.APP_HEIGHT));
 			}
 		});
 		
@@ -114,11 +104,35 @@ public class Summary extends VBox{
 				// Generate a list of hard questions
 				NumberList.generateHard();
 				// Move to the question scene
-				App.getMainStage().setScene(new Scene(new QuestionAsk(),App.APP_WIDTH,App.APP_HEIGHT));
+				App.getMainStage().setScene(new Scene(new QuestionAsk(),
+						App.APP_WIDTH,App.APP_HEIGHT));
 			}
 		});
 	}
 	
-	
+	private class SummaryBox extends VBox {
+		
+		private Label[] _labels;
+		
+		public SummaryBox() {
+			
+			setAlignment(Pos.CENTER);
+			_labels = new Label[10];
+			for (int i = 0; i < 10; i++) {
+				_labels[i] = new Label(NumberList.getNumberAt(i) + ": " + 
+						NumberList.getAnswerAt(i));
+				_labels[i].setScaleX(1.5);
+				_labels[i].setScaleY(1.5);
+				_labels[i].setFont(App.getRegFont());
+				if (NumberList.getAnswerAt(i).equals("Correct")) {
+					_labels[i].setTextFill(Color.web("#50B948"));
+				}
+				else {
+					_labels[i].setTextFill(Color.web("#CC0000"));
+				}
+				getChildren().add(_labels[i]);
+			}
+		}
+	}
 	
 }
