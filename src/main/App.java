5a08ae7f5f81356ee8791179dea3360b2bd1c1f4
Eugene <eugene.bulog@gmail.com>
2017-10-14 18:35:56 +1300

    Added multi-part (hard) question generation

--- src/main/App.java
+++ src/main/App.java
@@ -2,7 +2,6 @@ package main;
 
 public class App {
 	public static void main(String[] args) {
-		String[] test = utility.EquationList.generatePart(99);
-		System.out.println(test[0] + "\t\t" + test[1]);
+		utility.EquationList.generateHard(999);
 	}
 }
