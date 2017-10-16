ce8901bbf0603074825a1be22523c4c6d223ba00
Eugene-Bulog <eugene.bulog@gmail.com>
2017-10-16 13:55:58 +1300

    Added login screen

--- src/main/App.java
+++ src/main/App.java
@@ -152,7 +152,7 @@ public class App extends Application{
 			@Override
 			protected void succeeded() {
 				// Set up the main menu scene
-				_mainStage.setScene(new Scene(new MainMenu(), APP_WIDTH,APP_HEIGHT));
+				_mainStage.setScene(new Scene(new Login(), APP_WIDTH,APP_HEIGHT));
 				// Make the stage visible
 				_mainStage.show();
 				_mainStage.sizeToScene();
---
+++ src/main/Login.java
@@ -0,0 +1,88 @@
+package main;
+
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class Login extends VBox {
+	
+	private final Label TITLE;
+	private final Label ACCENT;
+	private final Label SUBTITLE;
+	private final TextField NAME;
+	private final Button CONTINUE;
+	
+	public Login() {
+		
+		// Set up background image
+		setBackground(App.getPatternBackground());
+		
+
+		// Set up accent for title
+		ACCENT = new Label("-     ");
+		ACCENT.setFont(App.getMaoriFont());
+		ACCENT.setTextFill(Color.web("#964B00"));
+		ACCENT.setPadding(new Insets(-200, 0, 0, 0));
+
+		
+		// Set up title and subtitle
+		TITLE = new Label("TATAI!");
+		TITLE.setFont(App.getMaoriFont());
+		TITLE.setTextFill(Color.web("#964B00"));
+		TITLE.setPadding(new Insets(-200, 0, 0, 0));
+		SUBTITLE = new Label("Please enter your name: ");
+		SUBTITLE.setScaleX(1.5);
+		SUBTITLE.setScaleY(1.5);
+		SUBTITLE.setFont(App.getRegFont());
+		
+		// Set up name textfield
+		NAME = new TextField();
+		NAME.setMaxWidth(200);
+		NAME.setScaleX(1.5);
+		NAME.setScaleY(1.5);
+		
+		// Set up continue button
+		CONTINUE = new Button("Continue");
+		CONTINUE.setScaleX(2);
+		CONTINUE.setScaleY(2);
+		CONTINUE.setFont(App.getRegFont());
+		setUpActions();
+		
+		// Set up Vbox and add children
+		setAlignment(Pos.CENTER);
+		setSpacing(40);
+		getChildren().add(ACCENT);
+		getChildren().add(TITLE);
+		getChildren().add(SUBTITLE);
+		getChildren().add(NAME);
+		getChildren().add(CONTINUE);
+		
+		
+	}
+	
+	/**
+	 * Sets up action listeners for all buttons in this scene
+	 */
+	private void setUpActions() {
+		
+		//Set up actions for continue button
+		CONTINUE.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// Goes to the main menu
+				App.getMainStage().setScene(new Scene(new MainMenu(),App.APP_WIDTH,App.APP_HEIGHT));
+			}
+			
+		});
+	}
+
+}
